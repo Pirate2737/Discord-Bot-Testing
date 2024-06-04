2b9d89ab749cf724b4c8a2c7239589af2e8c7ff1
Pirate2737 <hamzaalifarooqui@gmail.com>
2024-06-04 14:45:58 -0400

    hello

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,11 +1,10 @@
 import net.dv8tion.jda.api.JDABuilder;
-
 import javax.security.auth.login.LoginException;
 
 public class DiscordBot {
     public static void main(String[] args) throws LoginException {
-        JDABuilder jdaBuilder = JDABuilder.createDefault(System.getenv("token"));
+        JDABuilder api = JDABuilder.createDefault(System.getenv("token"));
 
-        jdaBuilder.build();
+        api.build();
     }
 }
