e0c648473add37affa063f598ec118a1108290cf
Pirate2737 <hamzaalifarooqui@gmail.com>
2024-08-20 15:58:25 -0400

    FIXED BIG ISSUE: 🛑 Slash commands disappeared b/c they were getting overwritten by the context menu commands 🟢 Fixed by combining the methods into "applicationCommands" --> improved formatting of it --> removed issue of ide warnings w slash commands

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -12,7 +12,6 @@ import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import net.dv8tion.jda.api.managers.Presence;
 import javax.security.auth.login.LoginException;
 import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
@@ -32,7 +31,7 @@ public class DiscordBot {
 
         JDABuilder api = JDABuilder.createDefault(System.getenv("token"));
 
-        api.addEventListeners(new Messenger(), new SlashCommands(), new Music(), new ContextMenu());
+        api.addEventListeners(new Messenger(), new SlashCommands(), new ContextMenu(), new Music());
         api.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS, GatewayIntent.DIRECT_MESSAGES);
         api.setMemberCachePolicy(MemberCachePolicy.ALL);
         api.setChunkingFilter(ChunkingFilter.ALL);
@@ -47,12 +46,7 @@ public class DiscordBot {
         }));
 
         readActivityFromJSON("status.json");
-        slashCommands();
-        contextMenuCommands();
-    }
-
-    public static JDA getJDA() {
-        return jda;
+        applicationCommands();
     }
 
     public static void readActivityFromJSON(String fileName) {
@@ -88,7 +82,6 @@ public class DiscordBot {
         }
     }
 
-    // set status method
     public static void setActivity(String activity, String status) {
         Presence presence = jda.getPresence();
 
@@ -101,62 +94,6 @@ public class DiscordBot {
         }
     }
 
-    // all the slash commands !!!!
-    public static void slashCommands() {
-        CommandListUpdateAction commands = jda.updateCommands(); // can take upwards of an hour
-        String botName = jda.getSelfUser().getName();
-
-        // say command
-        commands.addCommands (
-            Commands.slash("say", "Make " + botName + " say a message")
-                    .addOption(STRING, "content", "Message for the bot to repeat", true)
-                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_SEND))
-        );
-
-        // set status command
-        commands.addCommands(
-            Commands.slash("setactivity", "Change " + botName + "'s activity status")
-                    .addOptions(new OptionData(STRING,"activity", "the type of status",true)
-                            .addChoices(new Command.Choice("playing","playing"))
-                            .addChoices(new Command.Choice("competing in","competing"))
-                            .addChoices(new Command.Choice("listening to", "listening"))
-                            .addChoices(new Command.Choice("streaming", "streaming"))
-                            .addChoices(new Command.Choice("custom status", "custom")))
-                    .addOptions(new OptionData(STRING, "content", "Message to be displayed alongside the activity type", true)
-                            .setMaxLength(128))
-        );
-
-        // join voice channel
-        commands.addCommands(
-                Commands.slash("joinvc", "Have " + botName + " join a voice channel")
-                        .addOptions(new OptionData(CHANNEL, "vc", "Name of the voice channel", true)
-                                .setChannelTypes(ChannelType.VOICE))
-                        .setGuildOnly(true)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.VIEW_CHANNEL))
-        );
-
-        // leave voice channel
-        commands.addCommands(
-                Commands.slash("leavevc","Have " + botName + " leave the voice channel in this server")
-                        .setGuildOnly(true)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.VIEW_CHANNEL))
-        );
-
-        // source code
-        commands.addCommands(
-                Commands.slash("source", "View the source code for " + botName)
-        );
-
-        commands.queue();
-    }
-
-    public static void contextMenuCommands() {
-        jda.updateCommands().addCommands(
-                Commands.context(Command.Type.USER, "Get user avatar"),
-                Commands.message("Count words")
-        ).queue();
-    }
-
     public static void sendMessageToUser(Long userID, String message) {
         jda.retrieveUserById(userID).queue();
         User user = jda.getUserById(userID);
@@ -166,5 +103,62 @@ public class DiscordBot {
                 success -> System.out.println("Message sent successfully."),
                 failure -> System.err.println("Failed to send message: " + failure.getMessage())
         ));
+    }
+
+    public static void applicationCommands() {
+        String botName = jda.getSelfUser().getName();
+
+        jda.updateCommands().addCommands( // can take upwards of an hour
+            /*---------------------
+                 Slash Commands
+            ---------------------*/
+
+            // say command
+            Commands.slash("say", "Make " + botName + " say a message")
+                    .addOption(STRING, "content", "Message for the bot to repeat", true)
+                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MESSAGE_SEND)),
+
+            // set status command
+            Commands.slash("setactivity", "Change " + botName + "'s activity status")
+                    .addOptions(new OptionData(STRING,"activity", "the type of status",true)
+                            .addChoices(new Command.Choice("playing","playing"))
+                            .addChoices(new Command.Choice("competing in","competing"))
+                            .addChoices(new Command.Choice("listening to", "listening"))
+                            .addChoices(new Command.Choice("streaming", "streaming"))
+                            .addChoices(new Command.Choice("custom status", "custom")))
+                    .addOptions(new OptionData(STRING, "content", "Message to be displayed alongside the activity type", true)
+                            .setMaxLength(128)),
+
+            // join voice channel
+            Commands.slash("joinvc", "Have " + botName + " join a voice channel")
+                    .addOptions(new OptionData(CHANNEL, "vc", "Name of the voice channel", true)
+                            .setChannelTypes(ChannelType.VOICE))
+                    .setGuildOnly(true)
+                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.VIEW_CHANNEL)),
+
+            // leave voice channel
+            Commands.slash("leavevc","Have " + botName + " leave the voice channel in this server")
+                    .setGuildOnly(true)
+                    .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.VOICE_CONNECT, Permission.VOICE_SPEAK, Permission.VIEW_CHANNEL)),
+
+
+            // source code
+            Commands.slash("source", "View the source code for " + botName),
+
+            /*---------------------
+             Context Menu Commands
+            ---------------------*/
+
+            // Get User Avatar
+            Commands.context(Command.Type.USER, "Get user avatar"),
+
+            // Count Words
+            Commands.message("Count words")
+
+        ).queue();
+    }
+
+    public static JDA getJDA() {
+        return jda;
     }
 }
